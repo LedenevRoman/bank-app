6fbe1a1f374b7eaa047204131d8daf56447e7f2d
RomanLedenev <pommanbko@mail.ru>
2023-08-25 02:16:06 +0200

    fix mapstruct

--- src/main/java/com/training/rledenev/bankapp/mapper/AccountMapper.java
+++ src/main/java/com/training/rledenev/bankapp/mapper/AccountMapper.java
@@ -5,24 +5,26 @@ import com.training.rledenev.bankapp.entity.Account;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import org.mapstruct.NullValueCheckStrategy;
 
 import java.math.BigDecimal;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
-    @Mapping(source = "name", target = "name")
+
     @Mapping(source = "accountType", target = "accountType", qualifiedByName = "stringToUpperCase")
     @Mapping(source = "status", target = "status", qualifiedByName = "stringToUpperCase")
     @Mapping(source = "currencyCode", target = "currencyCode", qualifiedByName = "stringToUpperCase")
-    @Mapping(source = "balance", target = "balance", qualifiedByName = "doubleToBigDecimal")
+    @Mapping(source = "balance", target = "balance", qualifiedByName = "doubleToBigDecimal",
+            nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
     Account mapToEntity(AccountDto accountDto);
 
     @Named("stringToUpperCase")
     default String stringToUpperCase(String value) {
-        return value != null ? value.toUpperCase() : null;
+        return value.toUpperCase();
     }
     @Named("doubleToBigDecimal")
     default BigDecimal doubleToBigDecimal(Double balance) {
-        return balance != null ? BigDecimal.valueOf(balance) : null;
+        return BigDecimal.valueOf(balance);
     }
 }
